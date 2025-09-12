6ee7c284b407332350436973f6be9aac841795b9
Maxim Ivanov <qwoxieq@mail.ru>
2025-09-12 18:26:08 +0400

    added task 4 and refactored task 3

--- Lab_1/Maxim Ivanov/Task 4/myfirstpackage/MySecondClass.java
+++ Lab_1/Maxim Ivanov/Task 4/myfirstpackage/MySecondClass.java
@@ -1,22 +1,6 @@
-class MyFirstClass {
-    public static void main(String[] s) {
-        MySecondClass o = new MySecondClass();
-        System.out.println(o.div());
-        for (int i = 1; i <= 8; i++) {
-            for (int j = 1; j <= 8; j++) {
-                o.setFirst(i);
-                o.setSecond(j);
-                System.out.print(o.div());
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
+package myfirstpackage;
 
-    }
-}
-
-
-class MySecondClass {
+public class MySecondClass {
     private int first;
     private int second;
 
