b83cd364783f9a534cc273a40923282e5c758593
nikit <nikitakotenkov095@gmail.com>
2025-09-27 11:50:11 +0400

    realise Aitken method and add CompositeFunction class

--- Lab 2/src/functions/AitkenMethodFunction.java
+++ Lab 2/src/functions/AitkenMethodFunction.java
@@ -2,8 +2,31 @@ package functions;
 
 public class AitkenMethodFunction implements MathFunction{
 
+    private final MathFunction baseFunction;
+
+
+    public AitkenMethodFunction(MathFunction baseFunction) {
+        this.baseFunction = baseFunction;
+    }
+
+
     @Override
     public double apply(double x) {
-        return 0;
+        double x1 = baseFunction.apply(x);
+        double x2 = baseFunction.apply(x1);
+        double x3 = baseFunction.apply(x2);
+
+        return applyAitkenFormula(x1, x2, x3);
+    }
+
+
+    private double applyAitkenFormula(double x1, double x2, double x3) {
+        double denominator = x3 - 2 * x2 + x1;
+
+        if (Math.abs(denominator) < 1e-12) {
+            return x3;
+        }
+
+        return x1 - Math.pow(x2 - x1, 2) / denominator;
     }
 }
