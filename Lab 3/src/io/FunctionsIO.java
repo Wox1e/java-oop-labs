b869d0b7f8a9a43d18209665d9a9353196ade544
Maxim Ivanov <qwoxieq@mail.ru>
2025-10-11 22:38:34 +0400

    added file io

--- Lab 3/src/io/FunctionsIO.java
+++ Lab 3/src/io/FunctionsIO.java
@@ -1,7 +1,23 @@
 package io;
 
+import functions.Point;
+import functions.TabulatedFunction;
+
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
+    }
+    static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function){
+        PrintWriter printWriter = new PrintWriter(writer);
+
+        int count = function.getCount();
+        printWriter.println(count);
+        for (Point point:function){
+            printWriter.printf("%f %f\n", point.x, point.y);
+        }
+        printWriter.flush();
     }
 }
---
+++ Lab 3/src/io/TabulatedFunctionFileWriter.java
@@ -0,0 +1,29 @@
+package io;
+
+import functions.*;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TabulatedFunctionFileWriter {
+    public static void main(String[] args){
+        try (FileWriter arrayFileWriter = new FileWriter("output/array function.txt");
+            BufferedWriter arrayBufferedWriter = new BufferedWriter(arrayFileWriter);
+            FileWriter listFileWriter = new FileWriter("output/linked_list_function.txt");
+            BufferedWriter listBufferedWriter = new BufferedWriter(listFileWriter);
+            ){
+
+
+            double[] xVals = {1,4,5,9};
+            double[] yVals = {0,15,3,8};
+
+            TabulatedFunction arrayFunc = new ArrayTabulatedFunction(xVals, yVals);
+            TabulatedFunction listFunc = new LinkedListTabulatedFunction(xVals, yVals);
+            FunctionsIO.writeTabulatedFunction(arrayBufferedWriter, arrayFunc);
+            FunctionsIO.writeTabulatedFunction(listBufferedWriter, listFunc);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
