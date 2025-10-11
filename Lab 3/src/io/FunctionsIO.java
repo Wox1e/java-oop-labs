1be64d694062d99ecc208c2c7a66751bb244a393
Maxim Ivanov <qwoxieq@mail.ru>
2025-10-11 22:48:30 +0400

    fixed git collision :))

--- Lab 3/src/io/FunctionsIO.java
+++ Lab 3/src/io/FunctionsIO.java
@@ -3,8 +3,7 @@ package io;
 import functions.Point;
 import functions.TabulatedFunction;
 
-import java.io.BufferedWriter;
-import java.io.PrintWriter;
+import java.io.*;
 
 public final class FunctionsIO {
     private FunctionsIO() {
@@ -19,5 +18,15 @@ public final class FunctionsIO {
             printWriter.printf("%f %f\n", point.x, point.y);
         }
         printWriter.flush();
+    }
+
+    static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+        dataOutputStream.writeInt(function.getCount());
+        for(Point point : function) {
+            dataOutputStream.writeDouble(point.x);
+            dataOutputStream.writeDouble(point.y);
+        }
+        dataOutputStream.flush();
     }
 }
