c9d1384935ea5dca76f595a8a35bba53b8a389f2
nikit <nikitakotenkov095@gmail.com>
2025-11-25 15:47:22 +0400

    Added new dependencies and empty servlets

---
+++ src/main/java/com/oop/labs/manual/servlets/UserServlet.java
@@ -0,0 +1,37 @@
+package com.oop.labs.manual.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/users")
+public class UserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/plain");
+        PrintWriter out = resp.getWriter();
+        out.print("UserServlet is working!");
+        out.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Labs Manual</title>
+</head>
+<body>
+    <h1>Manual Tomcat Application - Working!</h1>
+    <p>If you see this, your application is deployed successfully!</p>
+    <h2>Test your servlets:</h2>
+    <ul>
+        <li><a href="users">Users Servlet</a></li>
+        <li><a href="functions">Functions Servlet</a></li>
+        <li><a href="points">Points Servlet</a></li>
+    </ul>
+</body>
+</html>
